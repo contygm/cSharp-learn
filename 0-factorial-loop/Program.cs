570eb196bde3b8526d5cb07385cac6a74a772405
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-01-06 15:34:23 -0800

    factorial in cshap: loops and recursion

---
+++ 0-factorial-loop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the nth value as an integer: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            // loop calculation
+            Console.WriteLine($"LOOP calculation: Foctorial of {n} is {CalculateLoop(n)}");
+        }
+
+        // LOOP: from 1 to n
+        private static int CalculateLoop(int n)
+        {
+            var factorial = 1;
+            for (int i = n; i >= 1; i--) 
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+
+        }
+
+        // LOOP: from n to 1
+        private static int CalculateLoop2(int n)
+        {
+            var factorial = n;
+            for (int i = 1; i < n; i++) 
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+
+        }
+
+         // RECURSION: from n to 1
+        private static int CalculateRecursive(int n)
+        {
+            // stops the recursion
+            if (n == 1)
+                return 1;
+
+            return n * CalculateRecursive(n - 1);
+        }
+    }
+}
