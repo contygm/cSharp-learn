15258cdc83fab08e393896f34ef7730891934a3d
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-01-06 16:29:22 -0800

    fibonacci loop working:

---
+++ 2-Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!! FIBONACCI !!");
+            Loop();
+        }
+
+        private static void Loop()
+        {
+            var num1 = 0;
+            var num2 = 1;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            
+            while (num1 < 100)
+            {
+                var num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+                Console.WriteLine(num3);
+            }
+        }
+    }
+}
