4d2f4a9cc93b2c32f99ba07c92bf064f23dd57af
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-01-06 16:50:38 -0800

    setting up tower of hanoi game

---
+++ 3-Tower-Of-Hanoi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tower_Of_Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        static void Tower(int n, int sourcePeg, int middlePeg, int destinationPeg)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(sourcePeg + "->" + destinationPeg);
+            }
+            else
+            {
+                
+            }
+
+        }
+    } 
+}
