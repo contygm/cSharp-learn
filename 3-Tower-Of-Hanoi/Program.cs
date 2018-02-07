e0cc8873c65b30b322e2d5fceef9cbf6103d0cae
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-02-07 12:39:01 -0800

    finished tower of hanoi

--- 3-Tower-Of-Hanoi/Program.cs
+++ 3-Tower-Of-Hanoi/Program.cs
@@ -4,20 +4,30 @@ namespace Tower_Of_Hanoi
 {
     class Program
     {
+        static int counter = 0;
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            // TODO: add directions for user
+            var discs = 0;
+            Console.WriteLine("Please enter the number of discs: ");
+            discs = Convert.ToInt32(Console.ReadLine());
+
+            Tower(discs, 1, 3, 2);
         }
         
-        static void Tower(int n, int sourcePeg, int middlePeg, int destinationPeg)
+        static void Tower(int n, int sourcePeg, int destinationPeg, int sparePeg)
         {
             if (n == 1)
             {
-                Console.WriteLine(sourcePeg + "->" + destinationPeg);
+                Console.WriteLine(counter + " " + sourcePeg + "->" + destinationPeg);
+                counter++;
             }
             else
             {
-                
+                Tower(n-1, sourcePeg, sparePeg, destinationPeg);
+                Console.WriteLine(counter + " " + sourcePeg + "->" + destinationPeg);
+                counter++;                
+                Tower(n-1, sparePeg, destinationPeg, sourcePeg);
             }
 
         }
